6d60d7607d81ac1b16b9f54a5e548f1353f637b5
Reznic854 <kfareznic@outlook.com>
2025-05-06 14:11:39 +1000

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TranslatorApp from './TranslatorApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TranslatorApp />
+  </React.StrictMode>
+);
